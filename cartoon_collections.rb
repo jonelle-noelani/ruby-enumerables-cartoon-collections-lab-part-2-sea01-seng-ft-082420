7ed742eeea6d5bd790dfcf254ee3a68e2cc01824
Jonelle Noelani Yacapin <jonelle_noelani@hotmail.com>
2020-08-14 07:28:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,6 @@ def find_valid_calls(planeteer_calls)
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
   # Return the first valid call found, or return nil if no valid calls are found
   result = planeteer_calls.find { |a| a.include?(valid_calls) }
-end
-result
+  puts result
 end
 end
