43af3d03b1fce02155347faaae9cf101cf72a7f2
Jonelle Noelani Yacapin <jonelle_noelani@hotmail.com>
2020-08-14 08:03:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,4 +25,4 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.find{ |this| this == valid_calls.each}
 end 
-end
+
