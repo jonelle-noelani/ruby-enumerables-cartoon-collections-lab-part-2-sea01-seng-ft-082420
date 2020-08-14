da49b4adb86270689c4adb66bc942c93493841cb
Jonelle Noelani Yacapin <jonelle_noelani@hotmail.com>
2020-08-14 08:05:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,6 +23,6 @@ def find_valid_calls(planeteer_calls)
 #valid_calls.each do  |element|
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
   # Return the first valid call found, or return nil if no valid calls are found
-  planeteer_calls.find{ |this| this == valid_calls.each |element| }
+  planeteer_calls.find{ |this| this == valid_calls.each}
 end 
 
